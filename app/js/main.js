4bc63b3b662da9ee7023b60762a486165690b766
nikkojustix <nickolaykotov@gmail.com>
2021-04-06 23:53:47 +0300

    v1.0 - desktop

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@ $(function () {
   $videoBtn  = $('.video__btn');
   $videoPlayer = $('.video__player');
   $video = $('.video');
+  $servicesBtn = $('.services__btn');
 
 
   $headerBtn.magnificPopup({
@@ -32,6 +33,113 @@ $(function () {
     $video.on('mouseleave', function() {
       $videoBtn.css('opacity', '0');
     });
-  }
+  };
+
+  $('.works__slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $servicesBtn.on('click', function(){
+    $(this).toggleClass('services__btn--opened')
+    .prev().children('.services__item-title').toggleClass('services__item-title--golden')
+    .next().toggleClass('services__price-list--opened');
+    // $(this).prev().children('.services__item-title').toggleClass('services__item-title--golden');
+    $(this).parent().toggleClass('services__item--opened');
+  });
+
+  $('.barbers__slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.feedback__slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
 })
